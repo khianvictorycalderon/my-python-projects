ed6fce6adb2b2fd2f76f2337051c69dfbe17adb6
Khian Calderon <ckhianvictory@gmail.com>
2024-11-28 23:16:26 +0800

    Activity 19 Changed

--- IT1A_1stSem_Activities/Activity19_Calderon_Khian.py
+++ IT1A_1stSem_Activities/Activity19_Calderon_Khian.py
@@ -1,1 +1,10 @@
-print(" This is Activity 19, Calderon.")
+
+print(" Enter 5 names, type stop to terminate names")
+
+for x in range(0,5):
+    ui = input(" Enter your name: ")
+    if ui.lower().strip() == "stop":
+        print(" Program terminated.")
+        break
+    else:
+        print(f" Hello {ui}")
