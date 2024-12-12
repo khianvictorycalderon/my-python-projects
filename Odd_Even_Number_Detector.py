2942661e1f941b6eafca42d8d080f5a72bc8514e
Khian Calderon <ckhianvictory@gmail.com>
2024-12-12 18:47:57 +0800

    Added Odd Even Number Detector

---
+++ Odd_Even_Number_Detector.py
@@ -0,0 +1,11 @@
+
+ui = int(input("Enter a number: "))
+
+if ui % 2 == 0:
+    print("Number is Even")
+else:
+    print("Number is Odd")
+    
+print("The sequence of numbers are: ")
+for number in range(ui-2, 0, -2):
+    print(number)
